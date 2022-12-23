177d504b1d42cc0d1c065d757ad8e0d98cbe8475
Suppakron <suwannapop01@gmail.com>
2022-12-23 15:27:02 +0700

    lab9_2oky

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,8 +3,33 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+double findGrade(double score)
+{
+	char grade ;
+	if (score >90)
+	{
+		grade ='A';
+	}else 
+	if (score >75 and score <= 90)
+	{
+		grade ='B';
 
+	}else if (score >60 and score <= 75)
+	{
+		grade ='C';;
+
+	}else if (score >45 and score <= 60)
+	{
+		grade ='D';
+
+	}else if (score <= 45)
+	{
+		grade ='F';
+
+	}
+return grade ;
+	
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
